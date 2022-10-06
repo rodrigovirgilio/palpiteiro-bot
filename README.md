b0108d7a9a60b7a6849792d130cf6d3e5d536b63
Matheus Couto <37025583+matheusccouto@users.noreply.github.com>
2022-10-06 02:06:56 +0000

    Update README

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-# palpiteiro-bot
+# Palpiteiro Bot
+Automatically submit line ups to Cartola using Palpiteiro Draft API.
+
+## Architecture
+![aws](https://img.shields.io/badge/Amazon_AWS-FF9900?logo=amazonaws&logoColor=white)
+![python](https://img.shields.io/badge/Python-FFD43B?logo=python&logoColor=blue)
+
+![architecture](diagrams/architecture.png)
 
 ## FAQ
 Q: How to get GLBID?
--- diagrams/make_diagrams.py
+++ diagrams/make_diagrams.py
@@ -7,14 +7,7 @@ import os
 from diagrams import Cluster, Diagram, Edge
 from diagrams.aws.compute import Lambda
 from diagrams.aws.integration import StepFunctions
-from diagrams.aws.network import APIGateway
-from diagrams.aws.storage import S3
 from diagrams.custom import Custom
-from diagrams.gcp.analytics import Bigquery
-from diagrams.onprem.ci import GithubActions
-from diagrams.onprem.client import User
-from diagrams.onprem.vcs import Git, Github
-from diagrams.programming.language import Python
 
 
 THIS_DIR = os.path.dirname(__file__)
@@ -22,7 +15,7 @@ ICONS_DIR = os.path.join(THIS_DIR, "icons")
 
 
 with Diagram(
-    filename=os.path.join(THIS_DIR, "overview"),
+    filename=os.path.join(THIS_DIR, "architecture"),
     show=False,
     curvestyle="curved",
 ):
@@ -39,19 +32,3 @@ with Diagram(
             >> Edge(label="post")
             >> cartola_salvar
         )
-
-
-with Diagram(
-    "\nFlow",
-    filename=os.path.join(THIS_DIR, "flow"),
-    show=False,
-    curvestyle="curved",
-):
-    gh_pr = Github("pull request to main")
-    gh_merge = Github("merge to main")
-    serverless = Custom("serverless", os.path.join(ICONS_DIR, "serverless.png"))
-
-    Git("push to dev") >> gh_pr
-    gh_pr >> GithubActions("testing") >> Python("pytest") >> gh_merge
-    gh_pr >> GithubActions("linting") >> Python("pylint") >> gh_merge
-    gh_merge >> GithubActions("deploy") >> serverless
